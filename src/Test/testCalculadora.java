dd4773f0863148d661041f4b414e9ddde45dec84
Jaime Garcia Marin <jaime.garcia1@alu.uhu.es>
2025-10-06 18:25:15 +0200

    TOD 1.PASO-Implementar Pruebas

---
+++ src/Test/testCalculadora.java
@@ -0,0 +1,29 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testCalculadora {
+
+	@Test
+	void testSuma() {
+		assertEquals(5, Calcuradora.suma(2,3));
+	}
+	@Test
+	void testResta() {
+		assertEquals(5, Calcuradora.resta(2,3));
+
+	}
+	@Test
+	void testMultiplica() {
+		assertEquals(5, Calcuradora.multiplica(2,3));
+
+	}
+	@Test
+	void testDivide() {
+		assertEquals(5, Calcuradora.divide(2,3));
+
+	}
+
+}
