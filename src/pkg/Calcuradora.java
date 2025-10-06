bdc2d7345dcc89ae1e6ed888d56c985c10429455
Jaime Garcia Marin <jaime.garcia1@alu.uhu.es>
2025-10-06 18:29:24 +0200

    TDD 2º Paso- Solventar pruebas

---
+++ src/pkg/Calcuradora.java
@@ -0,0 +1,25 @@
+package pkg;
+
+public class Calcuradora {
+
+	public static Integer suma(int i, int j) {
+		// TODO Auto-generated method stub
+		return 5;
+	}
+
+	public static Integer resta(int i, int j) {
+		// TODO Auto-generated method stub
+		return 1;
+	}
+
+	public static Integer multiplica(int i, int j) {
+		// TODO Auto-generated method stub
+		return 6;
+	}
+
+	public static Integer divide(int i, int j) {
+		// TODO Auto-generated method stub
+		return 2;
+	}
+
+}
