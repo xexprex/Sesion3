7b0e0ae8f2980e55efaef65cde47055500e0fd51
Jaime Garcia Marin <jaime.garcia1@alu.uhu.es>
2025-10-17 12:02:38 +0200

    Modificacion de cuenta, para ajustar datos.

--- src/pkg/Cuenta.java
+++ src/pkg/Cuenta.java
@@ -1,46 +1,64 @@
 package pkg;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cuenta {
 
-		String numero;
-		String titular;
-		Double saldo;
-	
-		//Constructor
-		public Cuenta(double saldo, String numero) {
-			this.saldo=saldo;
-			this.numero=numero;
-		}
-		
-		
-		public String getNumero() {
-			return numero;
-		}
-		public void setNumero(String numero) {
-			this.numero = numero;
-		}
-		public String getTitular() {
-			return titular;
-		}
-		public void setTitular(String titular) {
-			this.titular = titular;
-		}
-		public double getSaldo() {
-			return saldo;
-		}
-		public void setSaldo(Double saldo) {
-			this.saldo = saldo;
-		}
-		
-		
-		public void ingresar(Double i) {
-			saldo +=i ;			
-		}
-		
-		public void retirar(double cantidad) {
-		    if (saldo - cantidad >= -500)
-		        saldo -= cantidad;
-		    else
-		        System.out.println("Fondos insuficientes (saldo " + saldo + "€) en la cuenta " + numero + " para el retiro de " + cantidad);
-		}
+    private String numero;
+    private String titular;
+    private Double saldo;
+    private List<Movimiento> mMovimientos;
+
+    // Constructor
+    public Cuenta(double saldo, String numero) {
+        this.saldo = saldo;
+        this.numero = numero;
+        this.mMovimientos = new ArrayList<>();
+    }
+
+    // Getters y Setters
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+
+    public List<Movimiento> getMovimientos() {
+        return mMovimientos;
+    }
+
+    // Métodos de negocio
+    public void ingresar(Double importe) {
+        saldo += importe;
+        mMovimientos.add(new Movimiento(importe, Movimiento.Signo.H, "Ingreso de " + importe + "€"));
+    }
+
+    public void retirar(double cantidad) {
+        if (saldo - cantidad >= -500) {
+            saldo -= cantidad;
+            mMovimientos.add(new Movimiento(cantidad, Movimiento.Signo.D, "Reintegro de " + cantidad + "€"));
+        } else {
+            System.out.println("Fondos insuficientes (saldo " + saldo + "€) en la cuenta " + numero + " para el retiro de " + cantidad);
+        }
+    }
 }
+
